846969800aae21833663d54e74890de0dff47b56
Andreas Zetterlund <andzett@student.chalmers.se>
2022-11-25 10:43:49 +0100

    Task 1 final

--- Design Task2/Task1.m
+++ Design Task2/Task1.m
@@ -1,4 +1,6 @@
 %% Setup the car
+clc;
+clear;
 CAR.m = 1675; %Mass [kg]
 CAR.L = 2.675; % distance [m]
 CAR.g = 9.81; %Gravity constant
@@ -12,11 +14,11 @@ lf3 = 0.47;
 
 
 %Case 1
-Ku1 = CalcKu(lf1,CAR);
+Ku1 = CalcKu(lf1,CAR)
 %Case 2
-Ku2 = CalcKu(lf2,CAR);
+Ku2 = CalcKu(lf2,CAR)
 %Case 3
-Ku3 = CalcKu(lf3,CAR);
+Ku3 = CalcKu(lf3,CAR)
 
 %% Task 1.3: Critical/Characteristic speed (0.5 point)
 %For all load cases above, determine the critical and characteristic speeds 
@@ -38,8 +40,18 @@ df3 = ay*(CAR.L+Ku3*CAR.m*vx^2)/vx^2
 %Plot the steering wheel angle required vs speed curves for the three
 % cases for a curve radius of 200 m. Which of the load cases is 
 % understeer, oversteer and (relatively) neutral steer?
-
-
+clf;
+clc;
+Rp = 200;%[m] radius of corner
+vx = 0:0.1:100;
+df1 = GetSteeringAngle(vx,Rp,Ku1,CAR);
+df2 = GetSteeringAngle(vx,Rp,Ku2,CAR);
+df3 = GetSteeringAngle(vx,Rp,Ku3,CAR);
+hold on
+plot(vx,df1);
+plot(vx,df2);
+plot(vx,df3);
+legend('Ku1', 'Ku2','Ku3');
 
 
 %% Functions
@@ -50,8 +62,8 @@ end
 function vxChar = GetCharSpeed(Ku,CAR)
     vxChar = sqrt(CAR.L/(Ku*CAR.m));
 end
-function df = GetSteeringAngle(vx,ay,Ku,CAR)
-
+function df = GetSteeringAngle(vx,Rp,Ku,CAR)
+    df = (CAR.L/Rp + Ku*CAR.m.*vx.^2/Rp)*CAR.steeringRatio;
 end
 function Ku = CalcKu(lf1,CAR)
 c0 = 30.7;
@@ -64,5 +76,6 @@ L_r1 = CAR.L*(1-lf1);
 
 Cf1 = 2*(c0*F_fz1+c1*F_fz1^2);%Per axle
 Cr1 = 2*(c0*F_rz1+c1*F_rz1^2);%Per axle 
-Ku = CAR.m*CAR.g*(Cr1*L_r1-Cf1*L_f1)/(Cf1*Cr1*CAR.L);
+Ku = (Cr1*L_r1-Cf1*L_f1)/(Cf1*Cr1*CAR.L);
 end
+
