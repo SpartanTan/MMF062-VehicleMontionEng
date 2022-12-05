664691ae66e3663d29cab67522e7efff72cd4ae8
Andreas Zetterlund <andzett@student.chalmers.se>
2022-12-05 18:01:34 +0100

    Task 4.1

--- Design Task2/InitModel.m
+++ Design Task2/InitModel.m
@@ -19,7 +19,7 @@ vehicleData.R               = 0.3;                              % Wheel radius
 
 disp('Saab 9-3 parameters loaded');
 
-loadCase = 2    ;
+loadCase = 3    ;
 if loadCase==1
     caseLf=0.37;
     caseSWA= rad2deg(0.2950);
@@ -35,9 +35,9 @@ end
 vehicleData.lf              = caseLf*vehicleData.L;               % Distance of CoG from front axle
 vehicleData.lr              = vehicleData.L-vehicleData.lf;     % Distance of CoG from rear axle 
 simulationTime              = 10;                               % Simulation time
-vx0                         = 200;                              % Initial speed
-vehicleData.brakeDemand     = 0.0;                              % Brake force demand
-SWA                         = 3;                           % Steering wheel angle default = 10                                    
+vx0                         = 80;                              % Initial speed
+vehicleData.brakeDemand     = 0;                              % Brake force demand
+SWA                         = 0;                          % Steering wheel angle default = 10                                    
 vehicleData.pRollDist       = 0.65;                             % Front/Total roll stiffness distribution
 vehicleData.brakeDist       = 0.5;                              % Front/Total brake distribution ratio
 % end of file
